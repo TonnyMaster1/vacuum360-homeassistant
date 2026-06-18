d0aa1649e25022c79c928111618bc15587a326b5
Simon Latreille <latreille_simon@hotmail.com>
2026-06-18 16:46:18 -0400

    Add integration platform skeleton

---
+++ custom_components/vacuum360/button.py
@@ -0,0 +1,16 @@
+"""Platform placeholder for 360 Vacuum."""
+
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the platform."""
+    return
